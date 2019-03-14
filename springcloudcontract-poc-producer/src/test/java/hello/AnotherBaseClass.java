465d6226f646f2ebbdddb585fc38487da2dc7cd8
Daniel Adams <daniel.adams@cardinalhealth.com>
2019-03-14 10:17:27 -0400

    Adding another base class for demonstration

---
+++ springcloudcontract-poc-producer/src/test/java/hello/AnotherBaseClass.java
@@ -0,0 +1,27 @@
+package hello;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringcloudcontractPocProducerApplication.class)
+public abstract class AnotherBaseClass {
+
+        @Autowired PersonRestController personRestController;
+
+        @MockBean PersonService personService;
+
+        @Before public void setup() {
+                RestAssuredMockMvc.standaloneSetup(personRestController);
+
+                Mockito.when(personService.findPersonById(1L))
+                                .thenReturn(new Person(1L, "Daniel", "Adams"));
+        }
+
+}
